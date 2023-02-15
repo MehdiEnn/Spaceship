db739a5dc09a2510f0b70f365c44b9256b4eb76a
MehdiEnn <80798847+MehdiEnn@users.noreply.github.com>
2023-02-15 05:07:04 +0100

    Add files via upload

---
+++ SpaceshipOnMission.cs
@@ -0,0 +1,22 @@
+﻿using SpaceshipProperties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceshipOnMission
+{
+    class SpaceshipOnMission
+    {
+        public static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            Spaceship spaceship = new Spaceship("Spaceship", guid.ToString(), 50, 25, 15, Spaceship.Status.Dock.ToString());
+            spaceship.Launch();
+            spaceship.vaisseaux.Add(spaceship);
+            
+        }
+    }
+}
+
